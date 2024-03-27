c466fe4b319a428c9422d5fc34cff9f6a574233c
Chris Horne <chrhorne@deloitte.com>
2024-03-27 15:46:00 -0700

    Corrected endpoint logic

--- src/app.js
+++ src/app.js
@@ -5,6 +5,31 @@ const db = require("../db/connection");
 
 //TODO: Create your GET Request Route Below: 
 
+app.use(express.json());
+
+app.get("/restaurants", async(req, res) => {
+    const restaurants = await Restaurant.findAll();
+    res.json(restaurants)
+})
+app.get("/restaurants/:id", async(req, res) => {
+    const id = await Restaurant.findByPk(req.params.id);
+    res.json(id)
+})
+
+app.post("/restaurants", async (req, res) => {
+    const restaurant = Restaurant.create(req.body);
+    res.json(restaurant);
+})
+
+app.put("/restaurants/:id", async (req, res) => {
+    const updatedRest = await Restaurant.update(req.body, {where: {id: req.params.id}});
+    res.json(updatedRest);
+})
+app.delete("/restaurants/:id", async (req, res) => {
+    const deletedRest = await Restaurant.destroy({where: {id : req.params.id}});
+    res.json(deletedRest)
+});
+
 
 
 
